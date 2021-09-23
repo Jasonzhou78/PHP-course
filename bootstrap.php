c896a5feedb8983f8b97ea6231d7fed0fa0ca3e5
Jason <90533925+Jasonzhou78@users.noreply.github.com>
2021-09-23 15:26:22 -0400

    Add files via upload

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+  // Student: Zhijun Zhou
+  // Student#:0728193
+//bootstrap file
+require_once("config.php");
+?>
---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  // Student: Zhijun Zhou
+  // Student#:0728193
+
+  // Define the database connection constants here!
+ define('DB_HOST', 'localhost');
+ define('DB_USER', 'zzhou');     // your scweb username
+ define('DB_PASSWORD', 'wqs05wqs05ck0cgck0cg');  // See blackboard for 20-char password
+ define('DB_NAME', 'zzhoudb');   // username followed by lowercase db
+?>
